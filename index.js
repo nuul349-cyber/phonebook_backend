b8997f7514afb5a0162173fd59a005c377251396
Nuul <nuul@example.com>
2026-04-18 15:53:48 -0600

    step7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan("tiny"))
 
 let phoneBook = [
     { 
@@ -74,6 +76,12 @@ app.post('/api/persons', (req, res) => {
 
   res.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
