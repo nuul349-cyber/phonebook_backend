02e63fa9b1618e38c3a8140820c6ef1d90450202
Nuul <nuul@example.com>
2026-04-19 17:45:00 -0600

    uibuild

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
-app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
